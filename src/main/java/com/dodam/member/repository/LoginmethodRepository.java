ff7e372790a8eec1d5b5a91d2de74c4815dcd45e
Hansol <jjhansol9064@gmail.com>
2025-09-02 10:24:25 +0900

    Delete src directory

--- src/main/java/com/dodam/member/repository/LoginmethodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dodam.member.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dodam.member.entity.LoginmethodEntity;
-
-public interface LoginmethodRepository extends JpaRepository<LoginmethodEntity, Long> {
-    Optional<LoginmethodEntity> findByLmtype(String lmtype);
-}
