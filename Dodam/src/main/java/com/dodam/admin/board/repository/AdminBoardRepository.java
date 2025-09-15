c0abcc2e07a71860d2f0a9f70c62e9727c7efcab
junggyun <ppjg1217@gmail.com>
2025-09-15 09:24:28 +0900

    보드 커밋

--- Dodam/src/main/java/com/dodam/admin/board/repository/AdminBoardRepository.java
+++ Dodam/src/main/java/com/dodam/admin/board/repository/AdminBoardRepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+		
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+public interface AdminBoardRepository extends JpaRepository<BoardEntity, Long> {
 
     /**
      * 게시글 검색 및 필터링 (복합 조건)
