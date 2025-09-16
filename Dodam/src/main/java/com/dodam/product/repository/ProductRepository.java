1d9830603d27d63bd8694ad4fd55676cfb90485f
chan <chanbul7676@gmail.com>
2025-09-16 17:21:30 +0900

    상품작업 1차 완료

--- Dodam/src/main/java/com/dodam/product/repository/ProductRepository.java
+++ Dodam/src/main/java/com/dodam/product/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.dodam.product.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.dodam.product.entity.ProductEntity;
 
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>, JpaSpecificationExecutor<ProductEntity> {
+	
 }
