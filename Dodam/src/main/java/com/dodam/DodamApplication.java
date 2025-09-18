8d17d71f922ea77a2612946ff5d9939cb92a0b71
chan <chanbul7676@gmail.com>
2025-09-18 12:13:48 +0900

    application수정

--- Dodam/src/main/java/com/dodam/DodamApplication.java
+++ Dodam/src/main/java/com/dodam/DodamApplication.java
@@ -3,17 +3,13 @@ package com.dodam;
 import com.dodam.plan.config.PlanPortoneProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-=======
->>>>>>> refs/remotes/origin/chan787
+
 
 @SpringBootApplication
-<<<<<<< HEAD
 @EnableConfigurationProperties(PlanPortoneProperties.class) // ✅ properties 빈 등록
-=======
->>>>>>> refs/remotes/origin/chan787
+
 public class DodamApplication {
     public static void main(String[] args) {
         SpringApplication.run(DodamApplication.class, args);
