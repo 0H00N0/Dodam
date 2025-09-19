957365f2c2d2a7928f0adbf59b7652d8c054ce94
junggyun <ppjg1217@gmail.com>
2025-09-19 12:12:22 +0900

    보드 커밋

--- Dodam/src/main/java/com/dodam/board/service/NoticeService.java
+++ Dodam/src/main/java/com/dodam/board/service/NoticeService.java
@@ -1,16 +1,23 @@
 package com.dodam.board.service;
-import com.dodam.board.*; 
+
+import java.util.Date;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import com.dodam.board.service.BoardService;
 import com.dodam.board.dto.notice.*;
 import com.dodam.board.entity.BoardEntity;
 import com.dodam.board.entity.NoticeEntity;
 import com.dodam.board.error.NotFoundException; 
 import com.dodam.board.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.*; 
 import org.springframework.stereotype.Service; 
 import org.springframework.transaction.annotation.Transactional;
+
 @Service @RequiredArgsConstructor public class NoticeService {
   private final NoticeRepository repo; private final BoardService boards;
   @Transactional public NoticeResponse create(NoticeCreateRequest req){ BoardEntity b=boards.getByCodeOrThrow(req.getBoardCode());
