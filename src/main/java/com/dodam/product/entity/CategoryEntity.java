5d7ffabae0bc92f59ac5262cebfa2746b0bf1b9c
Park.JG <Park.JG@DESKTOP-78DBQQ0>
2025-09-19 20:07:55 +0900

    보드 커밋

--- src/main/java/com/dodam/product/entity/CategoryEntity.java
+++ src/main/java/com/dodam/product/entity/CategoryEntity.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-package com.dodam.product.entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Table(name = "category") // 카테고리 테이블 가정
-@Data
-public class CategoryEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long catenum;
-
-    @Column(length = 100)
-    private String catename; // 카테고리 이름
-}
-=======
 // CategoryEntity.java
 package com.dodam.product.entity;
 import jakarta.persistence.*; import lombok.*;
@@ -32,4 +13,4 @@ public class CategoryEntity {
   @Column(name="catename", nullable=false, length=100) 
   private String name;
 }
->>>>>>> refs/remotes/origin/chan787
+
