03398dd67943b45009be5e317477c86e2019904f
Won <weonpiljae@gmail.com>
2025-09-19 16:12:34 +0900

    commit

--- Dodam/src/main/java/com/dodam/rent/entity/RentEntity.java
+++ Dodam/src/main/java/com/dodam/rent/entity/RentEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class RentEntity {
+public class RentEntity { //대여 테이블
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
