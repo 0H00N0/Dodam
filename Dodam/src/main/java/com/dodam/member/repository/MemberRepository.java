83dfdeb17682beae2cb81064c310fc75cd40bfdf
HoonMo <72100081@gmail.com>
2025-09-25 14:04:59 +0900

    memberrepo

--- Dodam/src/main/java/com/dodam/member/repository/MemberRepository.java
+++ Dodam/src/main/java/com/dodam/member/repository/MemberRepository.java
@@ -6,7 +6,6 @@ import com.dodam.member.entity.MemberEntity;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 	boolean existsByMid(String mid);
-	Optional<MemberEntity> findByMid(String mid);
 	
 	// 이름+전화번호로 찾기
 	Optional<MemberEntity> findByMnameAndMtel(String mname, String mtel);
