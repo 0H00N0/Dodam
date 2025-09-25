40e6846e5b08d4adc8be16b75497048f725c9f8d
junggyun <ppjg1217@gmail.com>
2025-09-25 12:15:33 +0900

    보드 커밋

--- Dodam/src/main/java/com/dodam/board/dto/notice/NoticeResponse.java
+++ Dodam/src/main/java/com/dodam/board/dto/notice/NoticeResponse.java
@@ -1,16 +1,7 @@
 package com.dodam.board.dto.notice;
 import lombok.*; import java.time.*;
-
-import com.dodam.board.entity.NoticeEntity.NoticeEntityBuilder;
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builderpublic NoticeEntityBuilder boardCode(Class<? extends Long> class1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class NoticeResponse {
     private Long id; private String boardCode; private String title; private String content;
     private boolean pinned; private int views; private String status; private LocalDateTime createdAt; private LocalDateTime updatedAt;
-	public static NoticeEntityBuilder builder() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
